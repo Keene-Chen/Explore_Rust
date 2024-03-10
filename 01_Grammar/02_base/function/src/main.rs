bafc577071d80417027bbebecc382bdd78da7a40
KeeneChen <keenechen@qq.com>
2024-03-10 13:50:06 +0800

    refactor: 更新函数示例

--- 01_Grammar/02_base/function/src/main.rs
+++ 01_Grammar/02_base/function/src/main.rs
@@ -4,8 +4,16 @@ fn main() {
     // let c: i32 = add(a, b);
 
     println!("{}", add(a, b));
+    println!("{}", test());
 }
 
 fn add(a: i32, b: i32) -> i32 {
     a + b
+}
+
+/**
+ * 在rust中函数的返回值是块中最后一个表达式的值
+ */
+fn test() -> i32 {
+    6
 }
